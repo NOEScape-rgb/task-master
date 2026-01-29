b2c6e388a1933dd1a5410fff4fde83d1dda845d6
DoctorJunaid <junaid.aurangzeb1@gmail.com>
2026-01-29 10:51:55 +0500

    vercel.json

--- server.js
+++ server.js
@@ -28,8 +28,18 @@ const corsOptions = {
 // Apply CORS 
 app.use(cors(corsOptions));
 
-
-
+// Explicitly set CORS headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Max-Age", "3600");
+  
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
