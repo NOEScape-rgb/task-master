e1defadd2f6c5f05fda5dea83cd7990c47528cc7
Muhammad Junaid <junaid.aurangzeb1@gmail.com>
2026-02-14 20:32:58 +0500

    feat: Implement Cloudinary image upload middleware for profile pictures.

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,5 +1,5 @@
 const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
 const multer = require("multer");
 
 // check if cloudinary url is set in env
@@ -8,20 +8,20 @@ if (!process.env.CLOUDINARY_URL) {
 }
 
 
-const storage = new CloudinaryStorage({
+const cloudinaryStorage = require("multer-storage-cloudinary");
+
+const storage = cloudinaryStorage({
   cloudinary: cloudinary,
-  params: {
-    folder: "taskmaster_profiles",
-    allowed_formats: ["webp", "jpg", "png", "jpeg", "gif"],
-    transformation: [
-      {
-        width: 400,
-        height: 400,
-        crop: "fill",
-        gravity: "face",
-      },
-    ],
-  },
+  folder: "taskmaster_profiles",
+  allowedFormats: ["webp", "jpg", "png", "jpeg", "gif"],
+  transformation: [
+    {
+      width: 400,
+      height: 400,
+      crop: "fill",
+      gravity: "face",
+    },
+  ],
 });
 
 const upload = multer({
